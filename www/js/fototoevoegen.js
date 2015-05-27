6c013acf1180a84701a4337889cd06b37275c086
Bendegreef <ben.de.greef@student.ehb.Be>
2015-05-27 21:17:22 +0200

    s

--- www/js/fototoevoegen.js
+++ www/js/fototoevoegen.js
@@ -48,16 +48,15 @@ function onSuccess(imageData) {
 	var width;
 	
 	imageObj.src = "data:image/jpeg;base64," + imageData;
-
+	window.foto =  imageData;
 	width = imageObj.width; //breedte van afbeelding nemen
 	canvas.setAttribute('width', width); //canvas breedte instellen
 	canvas.setAttribute('height', width); //canvas hoogte instellen
 	context.drawImage(imageObj, 0, 0, width, width, 0, 0, width, width); //afbeelding tekenen
 	var dataURL = canvas.toDataURL(); //dataURL vullen 
-	window.foto =  dataURL;
 	document.getElementById("defImg").setAttribute('crossOrigin', 'anonymous');
 	document.getElementById("defImg").src = dataURL; //afbeelding toekennen
-       
+    window.foto = dataURL.replace("data:image/jpeg;base64,", "");
     }
     else if(/(iphone)|(ipad)/i.test(navigator.userAgent)){
         var image = document.getElementById('defImg');
